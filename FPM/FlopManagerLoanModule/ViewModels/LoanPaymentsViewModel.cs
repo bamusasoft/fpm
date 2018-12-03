fd62b87da3d4002ace179cbd0fbdedd56a32efde
bamusasoft <x.bamusa@gmail.com>
2018-12-03 23:52:07 +0300

    Loan Payments Added

---
+++ FPM/FlopManagerLoanModule/ViewModels/LoanPaymentsViewModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlopManager.Services;
+using FlopManager.Services.ViewModelInfrastructure;
+using FlopManagerLoanModule.DTOs;
+using FlopManagerLoanModule.Views;
+using Prism.Regions;
+
+namespace FlopManagerLoanModule.ViewModels
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class LoanPaymentsViewModel:EditableViewModelBase
+    {
+        private readonly ILogger _logger;
+        private readonly ISettings _settings;
+        private readonly LoanStatementDto _loanStatement;
+         
+        [ImportingConstructor]
+        public LoanPaymentsViewModel(ILogger logger, ISettings settings)
+        {
+            _logger = logger;
+            _settings = settings;
+            CanClose = true;
+            Errors = new Dictionary<string, List<string>>();
+            Title = ViewModelsTitles.LOAN_PAYMENTS;
+        }
+
+        #region Propertis
+
+        private string _loanNo;
+        public string LoanNo
+        {
+            get { return _loanNo; }
+            set { SetProperty(ref _loanNo, value); }
+        }
+
+        #endregion
+        #region "Base"
+        protected override void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Print()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Search(object criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void AddNew()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public override bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public override void OnNavigatedTo(NavigationContext navigationContext)
+        {
+
+            if (navigationContext.Parameters.Count() != 0)
+            {
+             string loanNo = navigationContext.Parameters["LoanNo"].ToString();
+              //Note: you can also pass object
+              LoanNo = loanNo;
+              //navigationContext.NavigationService.Region.Activate(navigationContext.Uri);
+            }
+
+        }
+
+        public override void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            
+        }
+
+        public override void OnStateChanged(ViewModelState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool IsValid()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+        [Import]
+        public IRegionManager RegionManager;
+    }
+}
---
+++ FPM/FlopManagerLoanModule/Views/LoanPaymentsView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FlopManagerLoanModule.ViewModels;
+
+namespace FlopManagerLoanModule.Views
+{
+    /// <summary>
+    /// Interaction logic for LoanPaymentsView.xaml
+    /// </summary>
+    [Export("LoanPaymentsView")]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public partial class LoanPaymentsView : UserControl
+    {
+       
+        public LoanPaymentsView()
+        {
+            InitializeComponent();
+        }
+
+        [Import]
+        public LoanPaymentsViewModel ViewModel
+        {
+            get { return DataContext as LoanPaymentsViewModel; }
+            set { DataContext = value; }
+        }
+    }
+}
+
